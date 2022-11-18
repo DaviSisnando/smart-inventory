6fad60976a2d34f326a486d854b9241883765527
Davi Sisnando <davi_sisnando@hotmail.com>
2022-11-18 17:59:55 -0300

    feat: Added required to InventoryCategory nome field

--- src/models/InventoryCategory.ts
+++ src/models/InventoryCategory.ts
@@ -14,7 +14,7 @@ interface InventoryCategoryFieldsDocument extends Document, InventoryCategoryFie
 }
 
 const InventoryCategorySchema: Schema = new Schema({
-    nome: String,
+    nome: [{type: String, required: true}],
     produtosRemessa: [{type: Types.ObjectId, ref: 'Shipment'}],
     produtos: [{type: Types.ObjectId, ref: 'Product'}],
     qtdTotoalProdutosCategoria: Number,
